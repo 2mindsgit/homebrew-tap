47b18f726d91a261f51e287222ab9c15f5a9ae91
2mindsgit <contact@2minds.fr>
2025-11-25 17:03:59 +0100

    Update sdcc.rb

--- Formula/sdcc.rb
+++ Formula/sdcc.rb
@@ -1,1 +1,41 @@
+class Sdcc < Formula
+  desc "ANSI C compiler for Intel 8051, Maxim 80DS390, and Zilog Z80"
+  homepage "https://sdcc.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/sdcc/sdcc/4.4.0/sdcc-src-4.4.0.tar.bz2"
+  sha256 "ae8c12165eb17680dff44b328d8879996306b7241efa3a83b2e3b2d2f7906a75"
+  license all_of: ["GPL-2.0-only", "GPL-3.0-only", :public_domain, "Zlib"]
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "boost" => :build
+  depends_on "gputils"
+  depends_on "readline"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "zstd"
+  end
+
+  on_system :linux, macos: :ventura_or_newer do
+    depends_on "texinfo" => :build
+  end
+
+  def install
+    system "./configure", "--disable-non-free", "--without-ccache", *std_configure_args
+    system "make", "install"
+    elisp.install bin.glob("*.el")
+  end
+
+  test do
+    (testpath/"test.c").write <<~C
+      int main() {
+        return 0;
+      }
+    C
+    system bin/"sdcc", "-mz80", "#{testpath}/test.c"
+    assert_path_exists testpath/"test.ihx"
+  end
+end
